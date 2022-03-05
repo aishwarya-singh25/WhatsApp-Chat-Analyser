ac52cbe1901cb3eb59a70dd1c9502dd62ba727db
khirodsahoo93 <khirod.sahoo93@gmail.com>
2022-03-05 13:30:14 -0800

    Made changes to DataStart method in WhatsApp text cleaner module

--- WhatsappChatAnalyser/whatsapp_text_cleaner.py
+++ WhatsappChatAnalyser/whatsapp_text_cleaner.py
@@ -12,7 +12,9 @@ import datetime
 # into the function itself
 #Function to check if the text is a new one or a continuation of old one.
 def DateStart(s):
-    pattern = '^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)(\d{2}|\d{4}), ([0-9][0-9]|[0-9]):([0-9][0-9])'
+    patterns = ['([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)(\d{2}|\d{4}), ([0-9][0-9]|[0-9]):([0-9][0-9])',
+    '([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)(\d{2}|\d{4}), ([0-9][0-9]|[0-9]):([0-9][0-9])']
+    pattern = '^' + '|'.join(patterns)
     result = re.match(pattern, s)
     if result:
         return True
