2c7c775144e653abbb8ccee24e39814ab5804c90
Anushka Singh <aqua@Anushkas-MacBook-Air.local>
2022-03-05 16:49:53 -0800

    Class files

---
+++ chat.py
@@ -0,0 +1,22 @@
+class Chat:
+  def __init__(authors, first_timestamp, last_timestamp, number_of_messages, sentiment):
+    self.authors = authors
+    self.number_of_messages = number_of_messages
+    self.sentiment = sentiment
+    self.first_timestamp = first_timestamp
+    self.last_timestamp = last_timestamp
+
+  def get_authors:
+  	return self._authors
+
+  def get_first_timestamp:
+  	return self._first_timestamp
+
+  def get_last_timestamp:
+  	return self._last_timestamp
+
+  def get_number_of_messages:
+  	return self._number_of_messages 
+
+  def get_sentiment:
+  	return self._sentiment
