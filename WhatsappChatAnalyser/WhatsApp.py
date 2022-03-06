eef157d0e98950966270b2f97b78583fa788a450
khirodsahoo93 <khirod.sahoo93@gmail.com>
2022-03-05 17:15:58 -0800

    New branch merged

---
+++ WhatsappChatAnalyser/WhatsApp.py
@@ -0,0 +1,4 @@
+filepath='/Users/stlp/Downloads/WhatsApp Chat with UW MSDS Fall21.txt'
+import WhatsappChatAnalyser as wp
+
+text=wp()
--- WhatsappChatAnalyser/WhatsappChatAnalyser.py
+++ WhatsappChatAnalyser/WhatsappChatAnalyser.py
@@ -1,8 +1,10 @@
 import whatsapp_text_cleaner as wtc
-
+filepath='/Users/stlp/Downloads/WhatsApp Chat with UW MSDS Fall21.txt'
+df=wtc.load_df(filepath)
 class text:
     def __init__(self,filepath,df=None):
        self.filepath=filepath
     def clean_text(self):
         return wtc.load_df(self.filepath)
 
+print(df.head())
