df23563bb9e1b75b36d4e823eeb4919019dd69f5
khirodsahoo93 <khirod.sahoo93@gmail.com>
2022-03-07 10:47:40 -0800

    kept only one class

--- WhatsappChatAnalyser/chat.py
+++ WhatsappChatAnalyser/chat.py
@@ -13,13 +13,118 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import whatsapp_chat_visualizer as wcv
 maskpath='/Users/stlp/Downloads/wordcloud trump.png'
 class Chat:
-  def __init__(self,message=None,name=None):
-    self.message = message
+  def __init__(self, name=None, Date=None , Time=None , Message=None ,Hours=None):
     self.name = name
+    self.Date = Date
+    self.Time = Time
+    self.Message = Message
+    self.Hours = Hours
 
+  # Update the attributes of the class using the dataframe created from text file
   def update_info(self,df):
     self.name = df['Author'].unique()
+    self.Date = df['Date'].unique()
+    self.Time = df['Time'].unique()
     self.Message = df['Message'].unique()
+    self.Hours = df['Hours'].unique()
+    
+  #Remove authors with None value which could be because of some activities in the group like deleting a text or updating group description
+  def remove_null_authors(self,df):
+    null_authors=df[df['Author'].isnull()]
+    df_new= df.drop(null_authors.index)
+    return df_new
+
+   #Find the most talkative person in the group 
+  def get_number_textMessages(self,df):
+    author_talkative=df['Author'].value_counts().sort_values(ascending=True)#count no. of messages per author
+    author_talkative_df=pd.DataFrame({'Author':author_talkative.index,'Messages_texted':author_talkative})
+    return author_talkative_df
+
+  #Find no. of medias shared by each individual i.e spammer
+  def get_number_mediasShared(self,df):
+    media_messages = df[df['Message'] == '<Media omitted>']
+    author_media_messages_value_counts = media_messages['Author'].value_counts().sort_values(ascending=True)
+    author_media_messages_value_counts_df=pd.DataFrame({'Author':author_media_messages_value_counts.index,'Media_shared':author_media_messages_value_counts})
+    return author_media_messages_value_counts_df
+
+  #Find the most confused individual of the group
+  def get_number_deletedMessges(self,df):
+    deleted_messages_df=df[df['Message']=='You deleted this message']
+    deleted_messages_df=deleted_messages_df.append(df[df['Message']=='This message was deleted'])
+    deleted_messages_counts=deleted_messages_df['Author'].value_counts().sort_values(ascending=True)
+    deleted_messages_counts_df=pd.DataFrame({'Author':deleted_messages_counts.index,'Messages_Deleted':deleted_messages_counts})
+    return deleted_messages_counts_df
+
+  #Group by Authors total no. of letters & total no. of words
+  def get_letters_words(self,df):
+    df['Letter_count']=df['Message'].apply(lambda s:len(s))
+    df['Word_count']=df['Message'].apply(lambda s:len(s.split(' ')))
+    group_df=df.groupby(['Author'])['Letter_count','Word_count'].sum().sort_values(by=['Word_count','Letter_count'],ascending=False)
+    group_df.columns=['Letter_count','Word_count']
+    return group_df
+
+  def get_startDate_endDate(self,df):
+    author_min_date=pd.DataFrame(df.groupby('Author')['Date'].min())#Find the first date of texting by the individual
+    author_max_date=pd.DataFrame(df.groupby('Author')['Date'].max())#Find the last date of texting by the individual
+    author_info=author_min_date.merge(author_max_date,on='Author', how='left')
+    author_info.rename(columns={'Date_x':'Start_date','Date_y':'Last_date'},inplace=True)
+    return author_info
+
+  #No. of active days in the group
+  def get_number_activeDays(self,df):
+    df_days_texted=pd.DataFrame(df.groupby('Author')['Date'].nunique()).rename(columns={'Date':'Days_texted'})
+    return df_days_texted
+
+  #No. of days in the group
+  def get_number_daysInGroup(self,df):
+    author_info=self.get_startDate_endDate(df)
+    author_info['Date_diff']=(author_info['Last_date']-author_info['Start_date']+datetime.timedelta(days=1)).dt.days.astype(int)
+    df_daysInGroup=author_info.drop(['Last_date','Start_date'],axis=1).rename(columns={'Date_diff':'daysInGroup'})
+    return df_daysInGroup
+
+
+  def get_stats(self,df):
+    self.update_info(df)
+    author_buffer_details=pd.DataFrame(data=self.name,columns=['Author'])
+    #leftjoin all data gathered
+    author_buffer_details=author_buffer_details.merge(self.get_number_activeDays(df),on='Author',how='left')#No. of active days in the group
+    author_buffer_details=author_buffer_details.merge(self.get_startDate_endDate(df),on='Author',how='left')#join first and Last texting date
+    author_buffer_details=author_buffer_details.merge(self.get_letters_words(df),on='Author',how='left')#join letters and words count
+    author_buffer_details=author_buffer_details.merge(self.get_number_mediasShared(df),on='Author',how='left')# join no. of media messages
+    author_buffer_details=author_buffer_details.merge(self.get_number_textMessages(df),on='Author',how='left')#join most talkative data
+    author_buffer_details=author_buffer_details.merge(self.get_number_deletedMessges(df),on='Author',how='left')#join deleted message data
+    author_buffer_details=author_buffer_details.merge(self.get_number_daysInGroup(df),on='Author',how='left')#No. of days in the group
+    return author_buffer_details
+
+
+  def get_aggressiveness(self,df):
+    author_buffer_details= self.get_stats(df)
+    print("\033[1m" + "Average number of messages per day for active days" + "\033[0m")
+    author_buffer_details['Agressiveness'] = (author_buffer_details['Messages_texted']/author_buffer_details['Days_texted']).round(2)
+    return author_buffer_details[['Author','Agressiveness']].sort_values(by='Agressiveness',ascending=False)
+   
+
+  def get_consistency(self,df):
+    author_buffer_details= self.get_stats(df)
+    print("\033[1m" + "Percentage of time texted atleast once since the start date " + "\033[0m")
+    author_buffer_details['Consistency'] = (100*author_buffer_details['Days_texted']/author_buffer_details['daysInGroup']).round()
+    return author_buffer_details[['Author','Consistency']].sort_values(by='Consistency',ascending=False)
+
+  def get_frequency(self,df):
+    author_buffer_details= self.get_stats(df)
+    print("\033[1m" + "Number of messages per day on whatsapp since the start date" + "\033[0m")
+    author_buffer_details['Frequency'] = ((author_buffer_details['Messages_texted']+author_buffer_details['Media_shared'])/author_buffer_details['daysInGroup'])
+    return author_buffer_details[['Author','Frequency']].sort_values(by='Frequency',ascending=False)
+
+  def get_metrics(self,df):
+    self.update_info(df)
+    author_buffer_details=pd.DataFrame({'Author':self.name})
+    print(author_buffer_details)
+    #leftjoin all data gathered
+    author_buffer_details=author_buffer_details.merge(self.get_consistency(df),on='Author',how='left')#consistency
+    author_buffer_details=author_buffer_details.merge(self.get_frequency(df),on='Author',how='left')#Frequency
+    author_buffer_details=author_buffer_details.merge(self.get_aggressiveness(df),on='Author',how='left')#Aggressiveness 
+    return  author_buffer_details
 
   #Remove stopWords and text coming from different sources
   def clean_text(self,message,extra_StopWords):
@@ -114,4 +219,17 @@ class Chat:
     author_buffer_details=pd.DataFrame(data=self.name,columns=['Author'])
     author_buffer_details=author_buffer_details.merge(df[['Author','avgWordspermessage','minWordspermessage',
     'maxWordspermessage','emovocab','totalemojis','top5emojis','vocab','top5words','words','Pos', 'Neg', 'Neu']].drop_duplicates(),on='Author',how='left')
-    return author_buffer_details    
+    return author_buffer_details  
+
+
+  
+
+
+  
+
+    
+        
+      
+
+
+    
--- WhatsappChatAnalyser/test.py
+++ WhatsappChatAnalyser/test.py
@@ -5,7 +5,6 @@
 
 
 print("hello user")
-import author
 import chat
 import whatsapp_text_cleaner as wtc
 import warnings
@@ -22,21 +21,17 @@ explode=[0.1,0,0.1,0.2,0.4]
 extra_StopWords = ["thats","dont","<Media omitted>","media","Media","Omitted","omitted","also","like","https","from","all","also","and","any","are","but","can","cant","cry","due","etc","few","for","get","had","has","hasnt","have","her","here","hers","herself","him","himself","his","how","inc","into","its","ltd","may","nor","not","now","off","once","one","only","onto","our","ours","out","over","own","part","per","put","see","seem","she","than","that","the","their","them","then","thence","there","these","they","this","those","though","thus","too","top","upon","very","via","was","were","what","when","which","while","who","whoever","whom","whose","why","will","with","within","without","would","yet","you","your","yours","the"]
 filepath='/Users/stlp/Downloads/WhatsApp Chat with UW MSDS Fall21.txt'
 text_df=wtc.load_clean_dataframe(filepath)
-A=author.Author()
 C=chat.Chat()
-A.update_info(text_df)
 C.update_info(text_df)
 #author_buffer_details=pd.DataFrame(data=A.name,columns=['Author'])
 #author_buffer_details=author_buffer_details.merge(A.get_number_activeDays(text_df),on='Author',how='left')
 #df_days_texted=pd.DataFrame(text_df.groupby('Author')['Date'].nunique()).rename(columns={'Date':'Days_texted'})
 #df=A.get_metrics(text_df)
-df_new=A.remove_null_authors(text_df)
-df=A.get_frequency(text_df)
+df_new=C.remove_null_authors(text_df)
 
-print(df)
 #wcv.bar_plot(df,max=5,sort=False)
 #wcv.pie(df,max=5,explode=explode)
-df=A.get_author_text_info(df_new,extra_StopWords,wordCloud=False)
+df=C.get_text_info(df_new,extra_StopWords,wordCloud=False)
 print(df.head())
 print("code ran succesfully")
 
