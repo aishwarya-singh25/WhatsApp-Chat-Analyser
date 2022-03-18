9fb3add075d15f1afcd2484bb5fb155d87975559
khirodsahoo93 <khirod.sahoo93@gmail.com>
2022-03-18 14:43:05 -0700

    Update test_group.py

--- WhatsappChatAnalyser/tests/test_group.py
+++ WhatsappChatAnalyser/tests/test_group.py
@@ -7,10 +7,11 @@ class UnitTests(unittest.TestCase):
 
     def test_get_text_info(self):
         g=group.Group()
-        df = pd.read_csv("Test_data.csv")
+        df = pd.read_csv("/Users/stlp/Documents/GitHub/WhatsappChatAnalyser/WhatsappChatAnalyser/tests/Test_data.csv")
         result = g.get_text_info(df, ["are", "hi"], False)
         self.assertTrue(len(result.columns), 13)
         return None
 
 if __name__ == '__main__':
     unittest.main()
+ 
